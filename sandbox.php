5fde67d9a6957b6419bafc6f6b2315d87ea644fe
TheGrums <lologrum10@gmail.com>
2017-02-12 17:41:35 +0100

    Testing into sandbox

--- sandbox.php
+++ sandbox.php
@@ -2,5 +2,7 @@
 
 ini_set("display_errors",true);
 require "./config/db.php";
-echo file_get_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'controllers/mail-top.html').file_get_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'controllers/default-mail-mid.php').file_get_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'controllers/mail-bot.html');
+session_start();
+sendMail($_SESSION['user'],"test","test");
+
 ?>
