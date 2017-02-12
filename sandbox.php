c33323938e276528caa9e52d8fa79562d0a817df
TheGrums <lologrum10@gmail.com>
2017-02-12 17:16:03 +0100

    Testing into sandbox

--- sandbox.php
+++ sandbox.php
@@ -2,6 +2,6 @@
 
 ini_set("display_errors",true);
 require "./config/db.php";
-echo file_get_contents(GENERAL_PATH.'controllers/mail-top.html').file_get_contents(GENERAL_PATH.'controllers/default-mail-mid.php').file_get_contents(GENERAL_PATH.'controllers/mail-bot.html');
+echo file_get_contents(dirname(__FILE__).DIRECTORY_SEPARATOR.'controllers/mail-top.html').file_get_contents(GENERAL_PATH.'controllers/default-mail-mid.php').file_get_contents(GENERAL_PATH.'controllers/mail-bot.html');
 echo password_hash("Ae4bDy67qxJ9",PASSWORD_BCRYPT,['cost' => 10]);
 ?>
